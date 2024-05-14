1274d005ed95aaa34d56ef007c095c759ca852f9
moon <57137100+qqsc-code@users.noreply.github.com>
2024-05-14 20:29:57 +1000

    Update calculator display and number button functionality

--- script.js
+++ script.js
@@ -30,3 +30,25 @@ function operate(num1, operator, num2) {
 let num1;
 let operator;
 let num2;
+
+let displayValue = 0;
+
+const screen = document.querySelector('.screen');
+
+function updatescreen() {
+    screen.textContent = displayValue;
+}
+
+const number = document.querySelectorAll('.number');
+number.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(displayValue == 0) {
+            displayValue = button.textContent;
+            num2 = button.textContent;
+        } else {
+            displayValue += button.textContent;
+            num2 = num2*10 + button.textContent;
+        }
+        updatescreen();
+    })
+});     
